23a42bac76a571c350156a5b949c83e4da504337
john Bradley <116833930+AJD254@users.noreply.github.com>
2023-10-23 03:59:31 +0300

    Add files via upload

---
+++ DOG YEARS/Dogyears.js
@@ -0,0 +1,9 @@
+//creat variable to store age and assigns value 25
+let myAge=25;
+const early_years=2*10.5; //creat variable signs value.
+let later_years=(myAge-2)*4;
+let myAgeInDogyears=early_years+later_years;
+let myName="Bradley";
+myName = myName.toUpperCase()//Changes value stored to lowercase.
+//displaying output using string interpolationbnb
+console.log(`My name is ${myName}.I am ${myAge} years in human years which is ${myAgeInDogyears} years old in dog years.`);
